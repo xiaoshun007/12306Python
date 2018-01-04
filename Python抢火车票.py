1b87fa2829711e68aae5a514621d29cee060a3b7
xiaoshun007 <495314431@qq.com>
2018-01-04 18:18:55 +0800

    Update Python抢火车票.py

--- Python抢火车票.py
+++ Python抢火车票.py
@@ -8,152 +8,152 @@ import traceback
 import time, sys
 
 class huoche(object):
-	"""docstring for huoche"""
-	driver_name=''
-	executable_path=''
-	#用户名，密码
-	username = u"xxx@qq.com"
-	passwd = u"xxx"
-	# cookies值得自己去找, 下面两个分别是上海, 太原南
-	starts = u"%u4E0A%u6D77%2CSHH"
-	ends = u"%u592A%u539F%2CTYV"
-	# 时间格式2018-01-19
-	dtime = u"2018-01-21"
-	# 车次，选择第几趟，0则从上之下依次点击
-	order = 0
-	###乘客名
-	users = [u"xxx"]
-	##席位
-	xb = u"二等座"
-	pz=u"成人票"
+    """docstring for huoche"""
+    driver_name=''
+    executable_path=''
+    #用户名，密码
+    username = u"xxx@qq.com"
+    passwd = u"xxx"
+    # cookies值得自己去找, 下面两个分别是武汉, 襄阳
+    starts = u"%u6B66%u6C49%2CWHN"
+    ends = u"%u8944%u9633%2CXFN"
+    # 时间格式2018-01-19
+    dtime = u"2018-01-11"
+    # 车次，选择第几趟，0则从上之下依次点击
+    order = 0
+    ###乘客名
+    users = [u"xxx"]
+    ##席位
+    xb = u"二等座"
+    pz=u"成人票"
 	
-	## 车次类型
-	train_types = [u"D", u"G"]
-	## 发车时间：时间格式
-	start_time = u"06:00--12:00"
+    ## 车次类型
+    train_types = ["D", "G"]
+    ## 发车时间：时间格式 12:00--18:00
+    start_time = u"12:00--18:00"
 
-	"""网址"""
-	ticket_url = "https://kyfw.12306.cn/otn/leftTicket/init"
-	login_url = "https://kyfw.12306.cn/otn/login/init"
-	initmy_url = "https://kyfw.12306.cn/otn/index/initMy12306"
-	buy="https://kyfw.12306.cn/otn/confirmPassenger/initDc"
-	login_url='https://kyfw.12306.cn/otn/login/init'
+    """网址"""
+    ticket_url = "https://kyfw.12306.cn/otn/leftTicket/init"
+    login_url = "https://kyfw.12306.cn/otn/login/init"
+    initmy_url = "https://kyfw.12306.cn/otn/index/initMy12306"
+    buy="https://kyfw.12306.cn/otn/confirmPassenger/initDc"
+    login_url='https://kyfw.12306.cn/otn/login/init'
 	
-	def __init__(self):
-		self.driver_name='chrome'
-		#self.executable_path='/usr/local/bin/chromedriver'
-		self.executable_path=r'C:\Users\sanshunfeng\Downloads\chromedriver.exe'
+    def __init__(self):
+        self.driver_name='chrome'
+        #self.executable_path='/usr/local/bin/chromedriver'
+        self.executable_path=r'C:\Users\sanshunfeng\Downloads\chromedriver.exe'
 
-	def login(self):
-		self.driver.visit(self.login_url)
-		self.driver.fill("loginUserDTO.user_name", self.username)
-		# sleep(1)
-		self.driver.fill("userDTO.password", self.passwd)
-		print(u"等待验证码，自行输入...")
-		while True:
-			if self.driver.url != self.initmy_url:
-				sleep(1)
-			else:
-				break
-
-	def preStart(self):
-		# 选择车次类型
-		for type in self.train_types
-			print(u'--------->选择车次类型', self.train_type)
-			# 车次类型选择
-			train_type_dict = {'T': u'T-特快',  			# 特快
-							   'G': u'GC-高铁/城际',  		# 高铁
-                               'D': u'D-动车',  			# 动车
-                               'Z': u'Z-直达'}  			# 直达
-			if train_type == 'T' or train_type == 'G' or train_type == 'D' or train_type == 'Z':
-				self.driver.find_by_text((train_type_dict[type]).click()
-			else:
-				print(u"车次类型异常或未选择!(train_type=%s)" % train_type)
+    def login(self):
+        self.driver.visit(self.login_url)
+        self.driver.fill("loginUserDTO.user_name", self.username)
+        # sleep(1)
+        self.driver.fill("userDTO.password", self.passwd)
+        print(u"等待验证码，自行输入...")
+        while True:
+            if self.driver.url != self.initmy_url:
+                sleep(1)
+            else:
+                break
+                
+    def preStart(self):
+        # 选择车次类型
+        for type in self.train_types:
+            print(u'--------->选择车次类型', type)
+            # 车次类型选择
+            train_type_dict = {'T': u'T-特快',                # 特快
+                                'G': u'GC-高铁/城际',         # 高铁
+                                'D': u'D-动车',               # 动车
+                                'Z': u'Z-直达'}               # 直达
+            if type == 'T' or type == 'G' or type == 'D' or type == 'Z':
+                self.driver.find_by_text(train_type_dict[type]).click()
+            else:
+                print(u"车次类型异常或未选择!(train_type=%s)" % type)
 		
-		# 选择发车时间
-		print(u'--------->选择发车时间', self.start_time)
-		self.driver.find_option_by_text(self.start_time).first.click()
+        # 选择发车时间
+        print(u'--------->选择发车时间', self.start_time)
+        self.driver.find_option_by_text(self.start_time).first.click()
 		
-	def start(self):
-		self.driver=Browser(driver_name=self.driver_name,executable_path=self.executable_path)
-		self.driver.driver.set_window_size(1400, 1000)
-		self.login()
-		# sleep(1)
-		self.driver.visit(self.ticket_url)
-		try:
-			print(u"购票页面开始...")
-			# sleep(1)
-			# 加载查询信息
-			self.driver.cookies.add({"_jc_save_fromStation": self.starts})
-			self.driver.cookies.add({"_jc_save_toStation": self.ends})
-			self.driver.cookies.add({"_jc_save_fromDate": self.dtime})
+    def start(self):
+        self.driver=Browser(driver_name=self.driver_name,executable_path=self.executable_path)
+        self.driver.driver.set_window_size(1400, 1000)
+        self.login()
+        # sleep(1)
+        self.driver.visit(self.ticket_url)
+        try:
+            print(u"购票页面开始...")
+            # sleep(1)
+            # 加载查询信息
+            self.driver.cookies.add({"_jc_save_fromStation": self.starts})
+            self.driver.cookies.add({"_jc_save_toStation": self.ends})
+            self.driver.cookies.add({"_jc_save_fromDate": self.dtime})
 
-			self.driver.reload()
+            self.driver.reload()
 
-			count=0
-			if self.order!=0:
-				while self.driver.url==self.ticket_url:
-					#self.driver.find_option_by_text("06:00--12:00").first.click()
-					#self.driver.find_by_text(u"GC-高铁/城际").click()
-					#self.driver.find_by_text(u"D-动车").click()
-					self.preStart()
-					sleep(0.05)
-					self.driver.find_by_text(u"查询").click()
-					count += 1
-					print(u"循环点击查询... 第 %s 次" % count)
-					# sleep(1)
-					try:
-						self.driver.find_by_text(u"预订")[self.order - 1].click()
-					except Exception as e:
-						print(e)
-						print(u"还没开始预订")
-						continue
-			else:
-				while self.driver.url == self.ticket_url:
-					#self.driver.find_option_by_text("06:00--12:00").first.click()
-					#self.driver.find_by_text(u"GC-高铁/城际").click()
-					#self.driver.find_by_text(u"D-动车").click()
-					self.preStart()
-					sleep(0.05)
-					self.driver.find_by_text(u"查询").click()
-					count += 1
-					print(u"循环点击查询... 第 %s 次" % count)
-					# sleep(0.8)
-					try:
-						for i in self.driver.find_by_text(u"预订"):
-							i.click()
-							sleep(0.5)
-					except Exception as e:
-						print(e)
-						print(u"还没开始预订 %s" %count)
-						continue
-			print(u"开始预订...")
-			# sleep(3)
-			# self.driver.reload()
-			sleep(1)
-			print(u'开始选择用户...')
-			for user in self.users:
-				self.driver.find_by_text(user).last.click()
+            count=0
+            if self.order!=0:
+                while self.driver.url==self.ticket_url:
+                    #self.driver.find_option_by_text("06:00--12:00").first.click()
+                    #self.driver.find_by_text(u"GC-高铁/城际").click()
+                    #self.driver.find_by_text(u"D-动车").click()
+                    self.preStart()
+                    sleep(0.05)
+                    self.driver.find_by_text(u"查询").click()
+                    count += 1
+                    print(u"循环点击查询... 第 %s 次" % count)
+                    # sleep(1)
+                    try:
+                        self.driver.find_by_text(u"预订")[self.order - 1].click()
+                    except Exception as e:
+                        print(e)
+                        print(u"还没开始预订")
+                        continue
+            else:
+                while self.driver.url == self.ticket_url:
+                    #self.driver.find_option_by_text("06:00--12:00").first.click()
+                    #self.driver.find_by_text(u"GC-高铁/城际").click()
+                    #self.driver.find_by_text(u"D-动车").click()
+                    self.preStart()
+                    sleep(0.05)
+                    self.driver.find_by_text(u"查询").click()
+                    count += 1
+                    print(u"循环点击查询... 第 %s 次" % count)
+                    # sleep(0.8)
+                    try:
+                        for i in self.driver.find_by_text(u"预订"):
+                            i.click()
+                            sleep(0.5)
+                    except Exception as e:
+                        print(e)
+                        print(u"还没开始预订 %s" %count)
+                        continue
+            print(u"开始预订...")
+            # sleep(3)
+            # self.driver.reload()
+            sleep(1)
+            print(u'开始选择用户...')
+            for user in self.users:
+                self.driver.find_by_text(user).last.click()
 
-			print(u"提交订单...")
-			sleep(0.5)
-			# self.driver.find_by_text(self.pz).click()
-			# self.driver.find_by_id('').select(self.pz)
-			# # sleep(1)
-			# self.driver.find_by_text(self.xb).click()
-			# sleep(1)
-			self.driver.find_by_id('submitOrder_id').click()
-			# print u"开始选座..."
-			# self.driver.find_by_id('1D').last.click()
-			# self.driver.find_by_id('1F').last.click()
+            print(u"提交订单...")
+            sleep(1)
+            # self.driver.find_by_text(self.pz).click()
+            # self.driver.find_by_id('').select(self.pz)
+            # # sleep(1)
+            # self.driver.find_by_text(self.xb).click()
+            # sleep(1)
+            self.driver.find_by_id('submitOrder_id').click()
+            # print u"开始选座..."
+            # self.driver.find_by_id('1D').last.click()
+            # self.driver.find_by_id('1F').last.click()
 
-			sleep(1)
-			print(u"确认选座...")
-			self.driver.find_by_id('qr_submit_id').click()
+            sleep(1)
+            print(u"确认选座...")
+            self.driver.find_by_id('qr_submit_id').click()
 
-		except Exception as e:
-			print(e)
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
-	huoche=huoche()
-	huoche.start()
+    huoche=huoche()
+    huoche.start()
