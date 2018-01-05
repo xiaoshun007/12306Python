7f1cfbc3c6c053c7f7e72477a9debae6e7b19343
xiaoshun <495314431@qq.com>
2018-01-05 21:41:47 +0800

    主程序支持k开头车次选择

--- hack12306.py
+++ hack12306.py
@@ -98,7 +98,9 @@ class huoche(object):
             train_type_dict = {'T': u'T-特快',                # 特快
                                 'G': u'GC-高铁/城际',         # 高铁
                                 'D': u'D-动车',               # 动车
-                                'Z': u'Z-直达'}               # 直达
+                                'Z': u'Z-直达'                # 直达
+                                'K': u'K-快速'                # 快速
+                                }
             if type == 'T' or type == 'G' or type == 'D' or type == 'Z':
                 print(u'--------->选择的车次类型', train_type_dict[type])
                 self.driver.find_by_text(train_type_dict[type]).click()
